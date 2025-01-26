ff7cd33f29816accf9d453ace3ace4353c390a49
Carla Gonzalez <carlafernanda006@gmail.com>
2025-01-26 14:58:38 -0500

    cambios

---
+++ Proyectos de C/programa1.c
@@ -0,0 +1,11 @@
+//Basic Poin
+
+//Directiva
+#include <stdio.h> //Preprocesador: Que incluye en el programa estandar de entrada y salida
+//Manda llamar la biblioteca de las directivas
+
+int main(){
+    printf("Hola Codigo C\n"); //Diagonal invertida caracter de escape. Nueva linea
+    return 0;
+}
+
