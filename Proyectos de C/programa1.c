7f854f7d36497fa18929887fb66933510f9f764f
Carla Gonzalez <carlafernanda006@gmail.com>
2025-01-26 15:11:31 -0500

    Probando c

--- Proyectos de C/programa1.c
+++ Proyectos de C/programa1.c
@@ -3,9 +3,21 @@
 //Directiva
 #include <stdio.h> //Preprocesador: Que incluye en el programa estandar de entrada y salida
 //Manda llamar la biblioteca de las directivas
+/*Funcion main inicia la ejecucion del programa*/
 
-int main(){
-    printf("Hola Codigo C\n"); //Diagonal invertida caracter de escape. Nueva linea
+/*fin de  la funcion main*/
+
+
+/*Funciones de suma*/
+int main (){
+    int num1;
+    int num2;
+    int resultado;
+    printf("Ingrese el primer valor\n");
+    scanf("%d", &num1);
+    printf("Ingrese el segundo valor\n");
+    scanf("%d", &num2);
+    resultado = num1 + num2;
+    printf("El resultado de la suma es:%d\n", resultado);
     return 0;
 }
-
